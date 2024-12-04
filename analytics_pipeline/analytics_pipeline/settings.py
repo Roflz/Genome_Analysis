0d553c5b8a70634b9d2b6b4849c5ec777fe3226a
Roflz <mahnriley@gmail.com>
2024-12-04 16:31:02 -0700

    remove password

--- analytics_pipeline/analytics_pipeline/settings.py
+++ analytics_pipeline/analytics_pipeline/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
         'ENGINE': "django.db.backends.postgresql",
         'NAME': 'data_analysis',
         'USER': 'postgres',
-        'PASSWORD': 'retard3',
+        'PASSWORD': '<your password>',
         'HOST': 'localhost',
         'PORT': '5432',
     }
